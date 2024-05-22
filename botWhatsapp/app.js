a495d599b6b491741218445780d17f338917a2e2
SSONARAKU <esthylucas1980@gmail.com>
2024-05-22 11:58:11 -0500

    chat bot menu

---
+++ botWhatsapp/app.js
@@ -0,0 +1,95 @@
+const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot')
+require("dotenv").config
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+const path = require("path")
+const fs = require("fs")
+const chat = require("./chatGPT")
+const { handlerAI } = require("./whisper")
+
+const menuPath = path.join(__dirname, "mensajes", "menu.txt")
+const menu = fs.readFileSync(menuPath, "utf8")
+
+const pathConsultas = path.join(__dirname, "mensajes", "promptConsultas.txt")
+const promptConsultas = fs.readFileSync(pathConsultas, "utf8")
+
+const flowVoice = addKeyword(EVENTS.VOICE_NOTE).addAnswer("Estoy escuchando la nota de voz, dame un momento", null, async (ctx, ctxFn) => {
+    const text = await handlerAI(ctx)
+    const prompt = promptConsultas
+    const consulta = text
+    const answer = await chat(prompt, consulta)
+    await ctxFn.flowDynamic(answer.content)
+})
+
+const flowMenuRest = addKeyword(EVENTS.ACTION)
+    .addAnswer('Este es el menu', {
+        media: "https://www.ujamaaresort.org/wp-content/uploads/2018/01/Ujamaa-restaurant-menu.pdf"
+    })
+
+const flowReservar = addKeyword(EVENTS.ACTION)
+    .addAnswer('Este es el flow reservas: ww.hacetureserva.com')
+
+
+const flowConsultas = addKeyword(EVENTS.ACTION)
+    .addAnswer('Este es el flow consultas')
+    .addAnswer("Hace tu consulta", { capture: true }, async (ctx, ctxFn) => {
+        const prompt = promptConsultas
+        const consulta = ctx.body
+        const answer = await chat(prompt, consulta)
+        await ctxFn.flowDynamic(answer.content)
+    })
+
+
+const flowWelcome = addKeyword(EVENTS.WELCOME)
+    .addAnswer("Este es el flujo Welcome", {
+        delay: 100,
+    },
+        async (ctx, ctxFn) => {
+            if (ctx.body.includes("Casas")) {
+                await ctxFn.flowDynamic("Escribiste casas")
+            } else {
+                await ctxFn.flowDynamic("Escribiste otra cosa")
+            }
+        })
+
+const menuFlow = addKeyword("Menu").addAnswer(
+    menu,
+    { capture: true },
+    async (ctx, { gotoFlow, fallBack, flowDynamic }) => {
+        if (!["1", "2", "3", "0"].includes(ctx.body)) {
+            return fallBack(
+                "Respuesta no válida, por favor selecciona una de las opciones."
+            );
+        }
+        switch (ctx.body) {
+            case "1":
+                return gotoFlow(flowMenuRest);
+            case "2":
+                return gotoFlow(flowReservar);
+            case "3":
+                return gotoFlow(flowConsultas);
+            case "0":
+                return await flowDynamic(
+                    "Saliendo... Puedes volver a acceder a este menú escribiendo '*Inicio*'"
+                );
+        }
+    }
+);
+
+const main = async () => {
+    const adapterDB = new MockAdapter()
+    const adapterFlow = createFlow([flowWelcome, menuFlow, flowMenuRest, flowReservar, flowConsultas, flowVoice])
+    const adapterProvider = createProvider(BaileysProvider)
+
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()
